3e11ab2213c5081298e03c0170e28e4664e3c490
yejinChoi <122140187+yejinchoi24@users.noreply.github.com>
2025-11-01 02:23:37 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 104.46 ms, Memory: 194 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,25 +1,32 @@
-import java.util.Stack;
-
+import java.util.*;//java에 내장된 모든 패키지 가져오기
 class Solution {
     public int[] solution(int[] numbers) {
-        int n = numbers.length;
-        int[] answer = new int[n];
-        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[numbers.length];
+        Arrays.fill(answer,-1);
         
-        for (int i = 0; i < n; i++) {
-            // 현재 숫자가 스택 top 인덱스의 값보다 크면, 뒤큰수 확정
-            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        
+        
+        for(int i=1;i<numbers.length;i++){
+            while(!stack.isEmpty()){//스택이 비어있을 때까지
                 int idx = stack.pop();
-                answer[idx] = numbers[i];
-            }
-            stack.push(i);
-        }
-
-        // 스택에 남은 인덱스는 뒤큰수가 없음
-        while (!stack.isEmpty()) {
-            answer[stack.pop()] = -1;
-        }
-
+                if(numbers[idx]<numbers[i]){//뒤가 더 클 때
+                    answer[idx] = numbers[i];
+                }else{//앞이 더 크거나 같을 때
+                    stack.push(idx);
+                    break;
+                    
+                }
+        }//while문 끝
+        stack.push(i);
+    }//for문 끝
+            
         return answer;
     }
 }
+//뒷큰수를 못찾은 원소의 인덱스 번호를 스택에 넣는다.
+//스택의 특징:후입선출LIFO,단방향 입출력 구조,데이터를 하나씩 넣고 뺄 수 있다.
+//1.numbers.length의 길이만큼 answer배열 길이 조정
+//2.answer 배열 -1 모두 초기화
+//3.stack선언->처음 인덱스 0을 넣는다
