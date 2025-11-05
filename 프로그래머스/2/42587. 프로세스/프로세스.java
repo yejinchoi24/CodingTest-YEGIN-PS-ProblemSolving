129378961a36da9634a3f2c9188827d9922aa000
yejinChoi <122140187+yejinchoi24@users.noreply.github.com>
2025-11-05 22:30:26 +0900

    [level 2] Title: 프로세스, Time: 11.48 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 1;
+        PriorityQueue p = new PriorityQueue<>(Collections.reverseOrder());;
+
+        for(int i=0; i<priorities.length; i++){
+            p.add(priorities[i]);
+            System.out.println(p);
+        }
+        System.out.println(p);
+
+        while(!p.isEmpty()){
+            for(int i=0; i<priorities.length; i++){
+                if(priorities[i] == (int)p.peek()){
+                    if(i == location){
+                        return answer;
+                    }
+                    p.poll();
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
